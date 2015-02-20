b76919edc60bd62c3b56d17a07720c78b84e7dfc
Hanna Seyoum <seyoumha@gmail.com>
2015-02-19 22:58:38 -0500

    email fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 RecipeApp::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  config.action_mailer.default_url_options = { :host => 'http://in2food.herokuapp.com' }
   # Code is not reloaded between requests.
   config.cache_classes = true
 
