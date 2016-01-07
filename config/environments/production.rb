66a05d280472edd960095d1f2a9cc66f6df3c452
Hanna <hanna@Toto.local>
2016-01-06 21:22:12 -0500

    exception notification

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,9 +63,9 @@ RecipeApp::Application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  #config.action_mailer.raise_delivery_errors = false
   config.action_mailer.delivery_method = :smtp
-  # config.action_mailer.perform_deliveries = true
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
@@ -82,7 +82,7 @@ RecipeApp::Application.configure do
   config.log_formatter = ::Logger::Formatter.new
   Rails.application.config.middleware.use ExceptionNotification::Rack,
   :email => {
-    :email_prefix => "[PREFIX] ",
+    :email_prefix => "[in2grub] ",
     :sender_address => %{"exceptions" <exceptions@in2grub.com>},
     :exception_recipients => %w{seyoumha@gmail.com}
   }
