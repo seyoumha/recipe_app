89c48de58142f2fe9c61a9d4e342331de8ef649a
Hanna <hanna@Toto.local>
2016-06-02 15:44:24 -0400

    config fix

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -1,5 +1,5 @@
 WickedPdf.config = {
   #:wkhtmltopdf => '/usr/local/bin/wkhtmltopdf',
   #:layout => "pdf.html",
-  :exe_path => '#{Rails.root}/.rvm/gems/ruby-2.2.2/bin/wkhtmltopdf'
+  :exe_path => '#{Rails.root}/bin/wkhtmltopdf'
 }
