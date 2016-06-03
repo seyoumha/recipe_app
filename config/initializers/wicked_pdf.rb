ed76ff3b9bf9ae4e0172a9cb27f38eac40e058d9
Hanna <hanna@Toto.local>
2016-06-03 14:58:44 -0400

    updated path 2

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -3,6 +3,11 @@
 #   #:layout => "pdf.html",
 #   :exe_path => '#{/usr/local/bin/wkhtmltopdf'
 # }
-WickedPdf.config = {
-  exe_path: '/usr/local/bin/wkhtmltopdf'
-}
+# 
+if Rails.env.production?
+  wkhtmltopdf_path = "#{Rails.root}/bin/wkhtmltopdf-amd64"
+else
+  wkhtmltopdf_path = "/Users/hanna/.rvm/gems/ruby-2.2.2/bin/wkhtmltopdf"            
+end
+ 
+WickedPdf.config = { exe_path: wkhtmltopdf_path, wkhtmltopdf: wkhtmltopdf_path }
