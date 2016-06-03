bb5ff0d8c8eaab51ae9d65b47c9719763217a81f
Hanna <hanna@Toto.local>
2016-06-03 14:42:44 -0400

    updated path

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -1,6 +1,8 @@
+# WickedPdf.config = {
+#   #:wkhtmltopdf => '/usr/local/bin/wkhtmltopdf',
+#   #:layout => "pdf.html",
+#   :exe_path => '#{/usr/local/bin/wkhtmltopdf'
+# }
 WickedPdf.config = {
-  #:wkhtmltopdf => '/usr/local/bin/wkhtmltopdf',
-  #:layout => "pdf.html",
-  :exe_path => '#{/usr/local/bin/wkhtmltopdf'
+  exe_path: '/usr/local/bin/wkhtmltopdf'
 }
-
